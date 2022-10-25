56f00562fbb5d5fc636e74d42262fe393d297a0c
h0llu <g.govzheev001@gmail.com>
2022-10-25 19:37:15 +0300

    Added generator modes

--- generator.py
+++ generator.py
@@ -1,28 +1,19 @@
 from configparser import ConfigParser
 from datetime import datetime
-from random import choice
+from random import choice, sample
 
 from kafka import KafkaProducer
 from numpy.random import normal
 
 
-def main():
-    PROPERTIES_FILE = "generator.properties"
-    CLIENT_FILE = "clients.txt"
+def generate_blacklist():
+    with open("blacklist.txt", "w") as f:
+        f.write("\n".join([str(x) for x in sample(range(1000000), 100000)]))
 
-    config_reader = ConfigParser()
-    config_reader.read(PROPERTIES_FILE)
-    config = {
-        "clients-amount": int(config_reader.get("DEFAULT", "clients-amount")),
-        "N": int(config_reader.get("DEFAULT", "messages-amount")),
-        "amount.mean": float(config_reader.get("amount", "mean")),
-        "amount.std": float(config_reader.get("amount", "std")),
-        "kafka.bootstrap-server": config_reader.get("kafka", "bootstrap-server"),
-        "kafka.topic": config_reader.get("kafka", "topic"),
-    }
 
+def send_to_kafka(config: dict):
     clients = []
-    with open(CLIENT_FILE, "r") as file:
+    with open("clients.txt", "r") as file:
         clients = file.read().splitlines()
 
     producer = KafkaProducer(bootstrap_servers=[config["kafka.bootstrap-server"]])
@@ -48,6 +39,27 @@ def main():
     print("ended at ", end_time)
     print("-" * 40)
     print(f"Sending {config['N']} messages took {end_time - start_time}")
+
+
+def main():
+    PROPERTIES_FILE = "generator.properties"
+
+    config_reader = ConfigParser()
+    config_reader.read(PROPERTIES_FILE)
+    config = {
+        "mode": config_reader.get("DEFAULT", "mode"),
+        "clients-amount": int(config_reader.get("DEFAULT", "clients-amount")),
+        "N": int(config_reader.get("DEFAULT", "messages-amount")),
+        "amount.mean": float(config_reader.get("amount", "mean")),
+        "amount.std": float(config_reader.get("amount", "std")),
+        "kafka.bootstrap-server": config_reader.get("kafka", "bootstrap-server"),
+        "kafka.topic": config_reader.get("kafka", "topic"),
+    }
+
+    if config["mode"] == "kafka":
+        send_to_kafka(config)
+    elif config["mode"] == "blacklist":
+        generate_blacklist()
 
 
 if __name__ == "__main__":
